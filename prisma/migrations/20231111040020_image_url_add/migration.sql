be8cc11b5bffd19b83e27862f1e607fcc066d084
RuanPerroni <rrplongo@gmail.com>
2023-11-11 01:16:49 -0300

    Core: fix aplication bug

---
+++ prisma/migrations/20231111040020_image_url_add/migration.sql
@@ -0,0 +1,51 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imgUrl` on the `Category` table. All the data in the column will be lost.
+  - You are about to drop the column `price` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `imageUrl` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `basePrice` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('WAITING_FOR_PAYMENT', 'PAYMENT_CONFIRMED');
+
+-- AlterTable
+ALTER TABLE "Category" DROP COLUMN "imgUrl",
+ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "price",
+ADD COLUMN     "basePrice" DECIMAL(8,2) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "OrderStatus" NOT NULL DEFAULT 'WAITING_FOR_PAYMENT',
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProduct" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,11 +1,54 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import Image from "next/image";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
-  return <h1>{product.name}</h1>;
+  return (
+    <div className="flex max-w-[156px] flex-col gap-4">
+      <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+          alt={product.name}
+        />
+      </div>
+
+      <div>
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
