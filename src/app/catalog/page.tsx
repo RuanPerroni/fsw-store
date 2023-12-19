acec5a8b3de913e91c846616917e73add586973f
RuanPerroni <rrplongo@gmail.com>
2023-12-18 21:07:22 -0300

    feat: add catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -7,9 +7,9 @@ const CatalogPage = async () => {
   const categories = await prismaClient.category.findMany({});
 
   return (
-    <div className="p-5">
+    <div className="flex flex-col gap-8 p-5">
       <Badge
-        className="gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
       >
         <ShapesIcon size={16} />
