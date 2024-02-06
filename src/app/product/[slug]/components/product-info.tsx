5797d66118367f0de45530418f345d3553776f73
RuanPerroni <rrplongo@gmail.com>
2024-02-06 20:37:49 -0300

    feat: add discount badge component

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -2,6 +2,7 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
 import {
   ArrowDownIcon,
@@ -34,22 +35,18 @@ const ProductInfo = ({
   return (
     <div className="flex flex-col px-5">
       <h2 className="text-lg">{name}</h2>
-
       <div className="flex items-center gap-2">
         <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
         {discountPercentage > 0 && (
-          <Badge className="px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {discountPercentage}%
-          </Badge>
+          <DiscountBadge>{discountPercentage}</DiscountBadge>
         )}
       </div>
-
+      .
       {discountPercentage > 0 && (
         <p className="text-sm line-through opacity-75">
           R$ {Number(basePrice).toFixed(2)}
         </p>
       )}
-
       <div className="mt-4 flex items-center gap-2">
         <Button
           size="icon"
@@ -69,16 +66,13 @@ const ProductInfo = ({
           <ArrowRightIcon size={16} />
         </Button>
       </div>
-
       <div className="mt-8 flex flex-col gap-3">
         <h3 className="font-bold">Descrição</h3>
         <p className="text-justify text-sm opacity-60">{description}</p>
       </div>
-
       <Button className="mt-8 font-bold uppercase">
         Adicionar ao carrinho
       </Button>
-
       <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
         <div className="flex items-center gap-2">
           <TruckIcon />
