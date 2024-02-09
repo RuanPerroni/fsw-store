81af213c1f97a1e01a58d548e42c2522c45a1146
RuanPerroni <rrplongo@gmail.com>
2024-02-08 21:10:53 -0300

    feat: ensure product can be removed from cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -15,6 +15,7 @@ interface ICartContext {
   addProductToCart: (Product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
+  removeProductFromCart: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -25,6 +26,7 @@ export const CartContext = createContext<ICartContext>({
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -83,6 +85,12 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
         return cartProduct;
       }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    setProducts((prev) =>
+      prev.filter((cartProduct) => cartProduct.id === productId),
     );
   };
 
